19c4b67a74d887fab5a32c430faa4e2533e30bdf
Skorlok <contact@skorlok.com>
2022-08-21 00:03:24 +0200

    second hotfix: added missing function in data access

--- Core/DataAccess.php
+++ Core/DataAccess.php
@@ -33,4 +33,49 @@ class DataAccess extends \ManiaLib\Utils\Singleton
     {
         $this->asyncCurl->query($url, $callback, $addionalData, $options);
     }
+
+    /** @todo make queue and process it onPostLoop */
+    final public function save($filename, $data, $append = false)
+    {
+        clearstatcache();
+        if (!is_file($filename)) {
+            if (!touch($filename)) {
+                chmod($filename, 0755);
+
+                return false;
+            }
+        }
+        clearstatcache();
+        if (is_writable($filename)) {
+            try {
+                if ($append === true) {
+                    return file_put_contents($filename, $data, LOCK_EX | FILE_APPEND);
+                }
+
+                return file_put_contents($filename, $data, LOCK_EX);
+            } catch (\Exception $e) {
+                Console::println("File write exception:" . $e->getMessage());
+
+                return false;
+            }
+        }
+    }
+
+    /** @todo make queue and process it onPostLoop */
+    final public function load($filename)
+    {
+        clearstatcache();
+        if (!is_file($filename)) {
+            return false;
+        }
+        if (is_readable($filename)) {
+            try {
+                return file_get_contents($filename);
+            } catch (\Exception $e) {
+                Console::println("File read exception:" . $e->getMessage());
+
+                return false;
+            }
+        }
+    }
 }
