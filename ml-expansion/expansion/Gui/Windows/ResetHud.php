7d39beb9f64b6a84b7fc97d11e22d45becb7a097
Skorlok <contact@skorlok.com>
2023-03-11 04:22:07 +0100

    autodelete old data fixed sheybug

--- ml-expansion/expansion/Gui/Windows/ResetHud.php
+++ ml-expansion/expansion/Gui/Windows/ResetHud.php
@@ -43,23 +43,6 @@ class ResetHud extends \ManiaLive\Gui\Window
                 EXP_widgetLayers.clear();
 
                 exp_needToCheckPersistentVars = True;
-
-
-                declare persistent Vec3[Text][Text] exp_windowLastPos;
-                declare persistent Vec3[Text][Text] exp_windowLastPosRel;
-
-                declare persistent Vec3[Text][Text][Text] eXp_widgetLastPos;
-                declare persistent Vec3[Text][Text][Text] eXp_widgetLastPosRel;
-                declare persistent Boolean[Text][Text][Text] eXp_widgetVisible;
-                declare persistent Text[Text][Text][Text] eXp_widgetLayers;
-
-                eXp_widgetVisible = ["deleted"=>["deleted"=>["deleted"=>True]]];
-                eXp_widgetLastPos = ["deleted"=>["deleted"=>["deleted"=>< 1.00, 1.00, 1.0>]]];
-                eXp_widgetLastPosRel = ["deleted"=>["deleted"=>["deleted"=>< 1.00, 1.00, 1.0>]]];
-                eXp_widgetLayers = ["deleted"=>["deleted"=>["deleted"=>"detete"]]];
-
-                exp_windowLastPos = ["deleted"=>["deleted"=>< 1.00, 1.00, 1.0>]];
-                exp_windowLastPosRel = ["deleted"=>["deleted"=>< 1.00, 1.00, 1.0>]];
             }
         --></script>');
         $this->addComponent($this->xml);
