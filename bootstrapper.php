8d9eb9ccbe8e5c22d394ecaa45dd8f86aa95683c
skorlok <contact@skorlok.com>
2023-12-29 21:13:49 +0100

    fix

--- bootstrapper.php
+++ bootstrapper.php
@@ -9,13 +9,12 @@ echo '
  _|      _|  _|    _|  _|    _|  _|  _|    _|  _|        _|  _|  _|    _|
  _|      _|    _|_|_|  _|    _|  _|    _|_|_|  _|_|_|_|  _|    _|        _|_|_|
 ';
-echo '---------------------------------------------------------------'.PHP_EOL;
+echo '--------------------------------------------------------------'.PHP_EOL;
 echo 'Running in Docker, skipping PHP Environment Compatibility Test'.PHP_EOL;
-echo '---------------------------------------------------------------'.PHP_EOL;
+echo '--------------------------------------------------------------'.PHP_EOL;
 
 date_default_timezone_set("Europe/Paris"); // Need to change this with a ENV var
 
-// enable error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 gc_enable();
