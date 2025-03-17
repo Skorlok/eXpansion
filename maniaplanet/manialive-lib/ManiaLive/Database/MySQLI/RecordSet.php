ba863d29ada9619b85840aa88ad9fff92e71488e
skorlok <contact@skorlok.com>
2025-03-17 06:15:52 +0100

    removed deprecated function

--- maniaplanet/manialive-lib/ManiaLive/Database/MySQLI/RecordSet.php
+++ maniaplanet/manialive-lib/ManiaLive/Database/MySQLI/RecordSet.php
@@ -39,14 +39,6 @@ class RecordSet extends \ManiaLive\Database\RecordSet
 		return mysqli_fetch_array($this->result, $resultType);
 	}
 	
-	/**
-	 * @deprecated
-	 */
-	function fetchStdObject()
-	{
-		return $this->fetchObject(null);
-	}
-	
 	function fetchObject($className='\\stdClass', array $params=array())
 	{
 		if($className)
