555f7cd5405ed71eaa1092fd7ea4e320c848a3c9
skorlok <contact@skorlok.com>
2025-07-12 00:55:15 +0200

    fixed WidgetButtons for php7.4

--- ml-expansion/expansion/Gui/Elements/WidgetButton.php
+++ ml-expansion/expansion/Gui/Elements/WidgetButton.php
@@ -13,7 +13,7 @@ class WidgetButton
 
         $y = 0.5;
         foreach ($text as $row) {
-            $xml .= '<label posn="0 ' . -($y * 3.2) . ' 2.0E-5" sizen="' . $sizeX - 2 . ' 3" halign="center" valign="center2" style="TextStaticSmall" textsize="1" textid="' . $row . '"/>';
+            $xml .= '<label posn="0 ' . (-($y * 3.2)) . ' 2.0E-5" sizen="' . ($sizeX - 2) . ' 3" halign="center" valign="center2" style="TextStaticSmall" textsize="1" textid="' . $row . '"/>';
             $y++;
         }
 
